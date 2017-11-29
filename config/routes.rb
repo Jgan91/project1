f65e6e74fb9901e134310a2e100836cec604c438
JustinGan <jgan91@gmail.com>
2017-11-30 08:36:45 +1100

    Revert "made a mistake adding users"
    This reverts commit 6d9e9ebacf5fda0115786d530fd473c9e98a24c7.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root :to => 'pages#home'
 
   resources :languages
   resources :countries
   resources :writing_systems
   resources :language_families
-
-  resources :users
 end
